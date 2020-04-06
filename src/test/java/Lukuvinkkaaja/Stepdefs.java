f0db0a92c0644dbbad23bf7f9e093c39c6c22883
Ilkka Ihalainen <ilkka.ihalainen@gmail.com>
2020-04-06 12:49:11 +0300

    cucumber testien aloittelua

---
+++ src/test/java/Lukuvinkkaaja/Stepdefs.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lukuvinkkaaja;
+
+import java.util.List;
+import ohtu.Lukuvinkkaaja.domain.LukuVinkki;
+import org.junit.Before;
+
+
+/**
+ *
+ * @author iilkka
+ */
+public class Stepdefs {
+    LukuVinkki lukuVinkki;
+    List<String> inputLines;
+    
+    
+    @Before
+    public void setup () {
+        lukuVinkki = new LukuVinkki("otsikko","www.lukuvinkki.com");
+    }
+    
+
+            
+            
+    
+}
